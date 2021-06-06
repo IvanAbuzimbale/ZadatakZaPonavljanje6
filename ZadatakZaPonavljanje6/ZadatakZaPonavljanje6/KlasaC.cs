03b1b091b2ce87878ba34a475dd5a7763981b2b1
IvanAbuzimbale <58472137+IvanAbuzimbale@users.noreply.github.com>
2021-06-06 16:02:02 +0200

    Add files via upload

---
+++ ZadatakZaPonavljanje6/ZadatakZaPonavljanje6/KlasaC.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_4
+{
+    class KlasaC
+    {
+        static public int Zbroj(int[] broj)
+        {
+            int Zbroj = broj.Sum();
+            return Zbroj;
+        }
+
+        static public double Prosjek(int[] broj)
+        {
+            double Prosjek = broj.Average();
+            return Prosjek;
+        }
+    }
+}
---
+++ ZadatakZaPonavljanje6/ZadatakZaPonavljanje6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] broj = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Unesite " + (i + 1) + ". broj: ");
+                broj[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Zbroj unesenih brojeva je " + KlasaC.Zbroj(broj));
+            Console.WriteLine("Prosjek unesenih brojeva je " + KlasaC.Prosjek(broj));
+            Console.ReadKey();
+        }
+    }
+}
